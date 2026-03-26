0bd4a46ed73dc286cefddcd0134b08c6bd641d28
manavisharma14 <manavisharma14@gmail.com>
2026-03-26 12:29:37 -0500

    streamlit threading frontend

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,14 +1,19 @@
+# from langgraph.graph import StateGraph, START, END
+# from typing import TypedDict, Annotated
+# from langchain_core.messages import BaseMessage
+# from langchain_openai import ChatOpenAI
+# from langgraph.checkpoint.memory import MemorySaver #stores in ram
+# from dotenv import load_dotenv
+
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
-from langgraph.checkpoint.memory import MemorySaver #stores in ram
+from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
-
 from langgraph.graph.message import add_messages
 
 class ChatState(TypedDict):
-
     messages: Annotated[list[BaseMessage], add_messages]
 
 load_dotenv()
@@ -17,24 +22,18 @@ llm = ChatOpenAI()
 
 def chat_node(state: ChatState):
 
-    # take user query from state
+    # take user query
     messages = state['messages']
-
-    # send to llm
     response = llm.invoke(messages)
-
-    # response store state
     return {'messages': [response]}
 
 checkpointer = MemorySaver()
 
 graph = StateGraph(ChatState)
-
 graph.add_node('chat_node', chat_node)
 
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
-
 
