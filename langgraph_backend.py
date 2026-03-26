8f4498f20c99eae5e0bd7ff23b787ab0a4e303e0
manavisharma14 <manavisharma14@gmail.com>
2026-03-26 09:27:25 -0500

    chatbot

---
+++ langgraph_backend.py
@@ -0,0 +1,40 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver #stores in ram
+from dotenv import load_dotenv
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+def chat_node(state: ChatState):
+
+    # take user query from state
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+checkpointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
